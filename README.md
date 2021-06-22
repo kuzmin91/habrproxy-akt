cc91f0540e55b4a41831117aca74a9cbfc53ce20
Alexander <aricarn2606@gmail.com>
2021-06-22 15:39:49 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+## Setup & Run
+pip install -r requirements.txt
+export FLASK_APP=app.py
+flask run
+Open http://127.0.0.1:5000/ru/ in your browser
---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, Response
+
+from habrproxy import process_proxy
+
+app = Flask(__name__)
+
+
+@app.route("/<path:path>", methods=['GET'])
+def main(path: str) -> Response:
+    app.logger.debug('processing proxy for {}'.format(path))
+    return process_proxy(request)
